439ad21950e29691cf3895c5ccf52f86c196a377
Jun <itomwu@gmail.com>
2018-08-16 22:16:16 +0800

    Add files via upload

---
+++ print.py
@@ -0,0 +1,24 @@
+x = 7 **6
+
+print (x)
+
+a = 10 # int
+
+b = 3.14 # float
+
+c = 3 # int
+
+d = a **2 # square of a
+
+print (type(d)) #return the type of d
+
+print (type(d//10)) # return the type of d/10
+
+print (type(a/b)) # return the type of a/b
+
+print (type(a/c)) # return the rype of a/c
+
+print (type(b*d)) # return the type of b*d
+
+
+
