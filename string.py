663b75433453d5f58aa27bc8d3f93e9032b879ef
mkls <git@wujun.org>
2018-08-16 21:57:51 +0800

    introduce usage of string

--- string.py
+++ string.py
@@ -1,5 +1,36 @@
-# test for string
+#  一串字符串（string）是字符（characters）的序列（sqquence）。基本上，字符串就是一串词汇。
+# 主要有单引号，双引号和三引号。引号的开始和结束必须是相同类型。
+# ''
+# ""
+# """ or ''' 主要用来指定多行的字符串
 
-a = "What a bad day!"
 
-print (a)
+print ('test')
+
+print("test")
+
+abc = '''
+Today is Aug 16 2018. I's a bad day. 
+
+I miss you.
+
+What are you doing now.
+
+'''
+
+print(abc)
+
+age = 20
+
+name = 'Swaroop'
+
+print('{0} was {1} years old when he wrote this book '. format(name,age))
+print('why is {0} playing with that python ?' .format(name))
+
+
+
+
+
+
+
+
