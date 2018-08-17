822d6d20f05ea3b146c1d6d66b1dcc8760ec7d1a
jun <git@wujun.org>
2018-08-17 15:20:38 +0800

    update

---
+++ Python Help/python help.md
@@ -0,0 +1,8 @@
+### python help usage
+
+参考：
+
+https://www.programiz.com/python-programming/methods/built-in/help
+https://docs.python.org/3.7/library/functions.html#help
+
+    help(object)
---
+++ Python Help/python help.py
@@ -0,0 +1,28 @@
+# help usage
+# help(object)
+
+# print(help(list.append))
+#
+#
+# print help on class list in module builtins
+#
+# print(help(list))
+#
+#
+# print(help("list"))
+
+
+# print(help(help))
+
+# print(help[1,2,3])
+
+# print(help('def'))
+
+
+from math import *
+
+print(help('math.pow'))
+
+
+
+
