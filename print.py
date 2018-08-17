3033ba385c7b98d1e9cad3c535cc921af614e284
jun <git@wujun.org>
2018-08-17 11:26:24 +0800

    update

--- print.py
+++ print.py
@@ -1,4 +1,25 @@
-<<<<<<< HEAD
+x = 7 **6
+
+print (x)
+
+a = 10 # int
+
+b = 3.14 # float
+
+c = 3 # int
+
+d = a **2 # square of a
+
+print (type(d)) #return the type of d
+
+print (type(d//10)) # return the type of d/10
+
+print (type(a/b)) # return the type of a/b
+
+print (type(a/c)) # return the type of a/c
+
+print (type(b*d)) # return the type of b*d
+
 x = 7 **6
 
 print (x)
@@ -23,29 +44,3 @@ print (type(b*d)) # return the type of b*d
 
 
 
-=======
-x = 7 **6
-
-print (x)
-
-a = 10 # int
-
-b = 3.14 # float
-
-c = 3 # int
-
-d = a **2 # square of a
-
-print (type(d)) #return the type of d
-
-print (type(d//10)) # return the type of d/10
-
-print (type(a/b)) # return the type of a/b
-
-print (type(a/c)) # return the rype of a/c
-
-print (type(b*d)) # return the type of b*d
-
-
-
->>>>>>> 439ad21950e29691cf3895c5ccf52f86c196a377
