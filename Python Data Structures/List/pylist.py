46283c6bfc9e25e3c4107bd2e8b6527cdde7105d
jun <git@wujun.org>
2018-08-17 15:44:43 +0800

    update

---
+++ Python Data Structures/List/pylist.py
@@ -0,0 +1,51 @@
+
+#list methods on a list object.
+
+# list = ['larry','curly','moe']
+#
+# list.append('shemp')
+#
+# print(list)
+#
+# list.insert(0,'xxx')
+#
+# print(list)
+#
+# list.extend(['yyy','zzz'])
+#
+# print(list)
+#
+# list.remove('curly')
+#
+# print(list)
+#
+# list.pop(1)
+#
+# print(list)
+
+
+# list = [1,2,3]
+#
+# print(list.append(4))
+#
+# list.append(4)
+#
+# print(list)
+
+
+# list = []
+#
+# list.append('a')
+#
+# list.append('b')
+#
+# print(list)
+
+
+# list1 = ['a', 'b', 'c','d']
+#
+# print((list1)[1:-1])
+#
+# list [0:2] = 'z'
+#
+# print(list1)
