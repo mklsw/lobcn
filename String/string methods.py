1004b35009172a2fe6e77cd45c4c8dc6fd1477ae
mkls <git@wujun.org>
2018-08-19 10:26:51 +0800

    update

---
+++ String/string methods.py
@@ -0,0 +1,21 @@
+# 字符串的大小写转换
+
+str1 = 'how are you ?'
+# upper 用来将字符串中的所有字符换成大写
+print(str1.upper())
+
+
+# lower 用来将字符串中的所有字符换成小写
+str2 = 'GOOD MORNING'
+
+print(str2.lower())
+
+# lower 和 upper  无视中间的大小写部分，全部生效
+
+str3 = 'How Are You?'
+
+print(str3.upper())
+
+
+
+
