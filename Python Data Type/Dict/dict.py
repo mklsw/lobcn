5cee38871e021f17380ce1288877f7a05738cd06
jun <git@wujun.org>
2018-08-23 16:15:23 +0800

    update

---
+++ Python Data Type/Dict/dict.py
@@ -0,0 +1,20 @@
+
+d = {'a':1,'b':2,'c':3}
+
+
+print(d)
+print(type(d))
+
+
+print(d['a'])
+
+
+print(len(d))  # 计算字典元素个数
+
+print(str(d))  #　输出字典可打印的字符串表示
+
+
+print(d.values())  # 以列表返回字典中的所有值
+
+print(d.popitem())
+
