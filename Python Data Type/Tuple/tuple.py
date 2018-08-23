133cc1e251f8c0c7e31112242c4adb31926106fd
jun <git@wujun.org>
2018-08-23 11:33:54 +0800

    update

--- Python Data Type/Tuple/tuple.py
+++ Python Data Type/Tuple/tuple.py
@@ -5,4 +5,14 @@ print(t[2])
 print(t[0:3])
 
 
-print(help(tuple))
+# print(help(tuple))
+
+a = (12,) # 在元素后面要加一个逗号
+print(a)
+print(type(a))
+
+b = (12) # 如果只是用括号括起来，而没有加逗号，不是元祖
+
+
+print(b)
+print(type(b))
