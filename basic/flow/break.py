825fc35fbe194bf0eae2f6b97863b071af0fb9b6
mkls <git@wujun.org>
2018-08-27 22:13:42 +0800

    update

---
+++ basic/flow/break.py
@@ -0,0 +1,10 @@
+while True:
+    s = input('Enter something:')
+
+    if s == 'quit':
+
+        break
+
+    print('Length of the string is ',len(s))
+
+print('Done')
--- basic/flow/if.py
+++ basic/flow/if.py
@@ -45,31 +45,7 @@
 # print('Done')
 #
 
-number = 90
 
-running = True
-
-while running:
-
-    guess = int(input('Enter an integer:'))
-
-    if guess == number:
-        print('Congratulations, You guessed it!')
-
-        running = False
-
-    elif guess < number:
-        print('No , it is a little higher than that.')
-
-    else :
-
-        print('No, it is a little lower than that.')
-
-else:
-
-    print('The while loop is over.')
-
-print('Done.')
 
 
 
